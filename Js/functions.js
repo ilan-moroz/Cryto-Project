7c643b2824cd829ef75cbfedb52930a446b02859
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-02-18 18:12:38 +0200

    modal

--- Js/functions.js
+++ Js/functions.js
@@ -43,7 +43,7 @@ const displayCoin = (coin, index) => {
       <div class="card-header border-warning d-flex justify-content-between">
         ${coin.symbol}
         <div class="form-check form-switch d-inline-block m-1 p-0">
-          <input class="form-check-input" id ="${checkBoxId}"type="checkbox"/>
+          <input class="form-check-input" id ="${checkBoxId}"type="checkbox" data-bs-toggle="modal" data-bs-target="#exampleModal"/>
         </div>
       </div>
       <div class="card-body">
@@ -150,21 +150,25 @@ const checkBoxCheck = (event) => {
       liveReportsArr.push(displayCoins[checkBoxIndex])
     } else {
       console.log('max 5')
-      $('body').append(`<div class="modal" tabindex="-1">
-      <div class="modal-dialog modal-dialog-centered">
+      $('body').append(`
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="modalVerticallyCenteredLabel">Modal title</h5>
-            <button type="button" class="btn-close" aria-label="Close"></button>
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body">...</div>
+          <div class="modal-body">
+            ...
+          </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary">Close</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
-    </div>`)
+    </div>
+    `)
     }
   } else {
     let arrIndex = liveReportsArr.indexOf(displayCoins[checkBoxIndex])
