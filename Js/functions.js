401dbe26e4f794bf413de02fe2057025544d0be3
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-23 10:20:32 +0300

    let to const

--- Js/functions.js
+++ Js/functions.js
@@ -1,45 +1,45 @@
 // APIS - CHANGED TO SHOW TOP 100 COINS
 const cryptoCoins =
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&sparkline=false'
-const cryptoInfo = 'https://api.coingecko.com/api/v3/coins/'
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&sparkline=false";
+const cryptoInfo = "https://api.coingecko.com/api/v3/coins/";
 
 // EMPTY ARRAY FOR THE DISPLAYED COINS AND LIVE REPORTS
-let displayCoins = []
-let liveReportsArr = []
+let displayCoins = [];
+let liveReportsArr = [];
 
 // FUNCTION fetchCoins GETS THE COINS FROM THE API
 const fetchCoins = async () => {
   return new Promise((resolve, reject) => {
     $.get({
       url: cryptoCoins,
-      success: (data) => {
-        resolve(data)
+      success: data => {
+        resolve(data);
       },
-      error: (error) => {
-        reject(error)
+      error: error => {
+        reject(error);
       },
-    })
-  })
-}
+    });
+  });
+};
 
 // FUNCTION getCoinsDisplay GETS 100 COINS FROM DATA AND RUNS displayCoin ON EACH COIN
 const getCoinsDisplay = async () => {
-  const data = await fetchCoins()
+  const data = await fetchCoins();
   for (let i = 0; i < 100; i++) {
-    displayCoins.push(data[i])
-    displayCoin(displayCoins[i], i)
+    displayCoins.push(data[i]);
+    displayCoin(displayCoins[i], i);
   }
-  return displayCoins
-}
+  return displayCoins;
+};
 
 // FUNCTION displayCoin FOR APPENDING THE COINS WITH BOOTSTRAP CARD
 const displayCoin = (coin, index) => {
   // MAKE DIFFERENT ID FOR EACH COLLAPSE
-  let targetId = `coin-${index}-details`
+  const targetId = `coin-${index}-details`;
   // MAKE DIFFERENT ID FOR EACH CHECKBOX
-  let checkBoxId = `checkbox-${index}`
+  const checkBoxId = `checkbox-${index}`;
   // APPEND THE CARD THE COINS DIV
-  $('.coins').append(`
+  $(".coins").append(`
     <div class="card border-warning text-warning bg-transparent ">
       <div class="card-header border-warning d-flex justify-content-between">
         ${coin.symbol}
@@ -57,117 +57,117 @@ const displayCoin = (coin, index) => {
         </div>
       </div>
     </div>
-  `)
-}
+  `);
+};
 
 // FUNCTION filterCoins FOR DISPLAYING THE COINS USER SEARCHES
 const filterCoins = () => {
   // THE COINS THE USER SEARCHES
-  let searchTerm = $('.search').val().toLowerCase()
+  const searchTerm = $(".search").val().toLowerCase();
   // IF EMPTY SEARCH SHOW ALL COINS
   if (!searchTerm) {
-    $('.card').show()
-    return
+    $(".card").show();
+    return;
   }
   for (let index = 0; index < displayCoins.length; index++) {
     // FIND THE COIN SPECIFIC INDEX
-    let card = $(`.card:eq(${index})`)
+    const card = $(`.card:eq(${index})`);
     // SHOW ALL COINS THAT EQUAL THE SEARCH VALUE ONLY SYMBOL
     if (displayCoins[index].symbol.toLowerCase() !== searchTerm) {
-      card.hide()
+      card.hide();
     } else {
-      card.show()
+      card.show();
     }
   }
-}
+};
 
 // FUNCTION parallax FOR SCROLLING
 const parallax = () => {
   $(window).scroll(function () {
-    let scroll = $(window).scrollTop()
-    $('.parallax').css({
-      'background-position-y': -scroll * 0.5 + 'px',
-    })
-  })
-}
+    const scroll = $(window).scrollTop();
+    $(".parallax").css({
+      "background-position-y": -scroll * 0.5 + "px",
+    });
+  });
+};
 
 // FUNCTION fetchCoinsData GETS A SPECIFIC COIN DATA FROM THA API AND SAVE IN CACHE FOR 2 MIN
-const fetchCoinsData = async (coinId) => {
-  const cacheKey = `coin_${coinId}`
-  const cache = await caches.open('coinsCache')
-  const cachedResponse = await cache.match(cacheKey)
+const fetchCoinsData = async coinId => {
+  const cacheKey = `coin_${coinId}`;
+  const cache = await caches.open("coinsCache");
+  const cachedResponse = await cache.match(cacheKey);
   // IF RESPONSE IS IN CACHE RESOLVE FROM CACHE AND NOT API
   if (cachedResponse) {
-    const cachedData = await cachedResponse.json()
-    return Promise.resolve(cachedData)
+    const cachedData = await cachedResponse.json();
+    return Promise.resolve(cachedData);
   }
   // IF RESPONSE NOT IN CACHE RESOLVE FROM API AND SAVE TO CACHE
   return new Promise((resolve, reject) => {
     $.get({
       url: cryptoInfo + coinId,
-      success: (data) => {
+      success: data => {
         const response = new Response(JSON.stringify(data), {
-          headers: { 'Cache-Control': 'max-age=120' },
-        })
-        cache.put(cacheKey, response)
-        resolve(data)
+          headers: { "Cache-Control": "max-age=120" },
+        });
+        cache.put(cacheKey, response);
+        resolve(data);
         setTimeout(() => {
-          cache.delete(cacheKey) // DELETE CACHE AFTER 2 MIN
-        }, 120000)
+          cache.delete(cacheKey); // DELETE CACHE AFTER 2 MIN
+        }, 120000);
       },
-      error: (error) => {
-        reject('Error while fetching details about the coin', error)
+      error: error => {
+        reject("Error while fetching details about the coin", error);
       },
-    })
-  })
-}
+    });
+  });
+};
 
 // FUNCTION getCoinDetailsFromClick GET ID AND INDEX OF CLICKED COIN
-const getCoinDetailsFromClick = (event) => {
-  let coinIndex = $(event.target).closest('.card').index()
-  let coinId = displayCoins[coinIndex].id.toLowerCase()
-  coinData(coinId, coinIndex)
-}
+const getCoinDetailsFromClick = event => {
+  const coinIndex = $(event.target).closest(".card").index();
+  const coinId = displayCoins[coinIndex].id.toLowerCase();
+  coinData(coinId, coinIndex);
+};
 
 // FUNCTION coinData GETS SPECIFIC COIN DATA FROM THE API AND APPEND TO COLLAPSE
 const coinData = async (coinId, coinIndex) => {
-  let coinData = await fetchCoinsData(coinId)
-  let targetId = `coin-${coinIndex}-details`
+  const coinData = await fetchCoinsData(coinId);
+  const targetId = `coin-${coinIndex}-details`;
   $(`#${targetId}`).html(`
   <img class="collapseImg" src="${coinData.image.large}"/>
   USD:<br/> 1 ${coinData.name} = ${Number(
-    coinData.market_data.current_price.usd,
+    coinData.market_data.current_price.usd
   )} $<br/>
   EURO:<br/>  1 ${coinData.name} = ${Number(
-    coinData.market_data.current_price.eur,
+    coinData.market_data.current_price.eur
   )} €<br/>
   ILS:<br/>  1 ${coinData.name} = ${Number(
-    coinData.market_data.current_price.ils,
-  )} ₪`)
-}
+    coinData.market_data.current_price.ils
+  )} ₪`);
+};
 
 //FUNCTION checkBoxCheck ADDING AND REMOVING CHECKED COINS TO liveReportsArr ARRAY
-const checkBoxCheck = (event) => {
-  let checkBoxId = event.target.id
-  let checkBoxIndex = checkBoxId.split('-')[1]
-  if ($('#' + checkBoxId).is(':checked')) {
+const checkBoxCheck = event => {
+  const checkBoxId = event.target.id;
+  const checkBoxIndex = checkBoxId.split("-")[1];
+  if ($("#" + checkBoxId).is(":checked")) {
     if (liveReportsArr.length < 6) {
-      liveReportsArr.push(displayCoins[checkBoxIndex])
+      liveReportsArr.push(displayCoins[checkBoxIndex]);
     }
   } else {
-    let arrIndex = liveReportsArr.indexOf(displayCoins[checkBoxIndex])
-    liveReportsArr.splice(arrIndex, 1)
+    let arrIndex = liveReportsArr.indexOf(displayCoins[checkBoxIndex]);
+    liveReportsArr.splice(arrIndex, 1);
   }
-  checkBoxCont()
+  checkBoxCont();
   // // add coins to live Reports chart
   // getCoinsPriceChart()
-}
+};
 
 // FUNCTION checkBoxCont ADD ALL SELECTED COINS TO ARRAY AND INJECT TO MODAL(SHOW MODAL AFTER 6 COINS SELECTED)
 const checkBoxCont = () => {
-  let howMuch = $('.form-check-input').filter(':checked').length
+  const howMuch = $(".form-check-input").filter(":checked").length;
   if (howMuch >= 6) {
-    $('.modal-body').html(`
+    $(".modal-body").html(`
     <div class="row">
     <div class="col-6">
       <h5>Live Report Coins</h5>
@@ -186,130 +186,130 @@ const checkBoxCont = () => {
       </ul>
     </div>
   </div>
-    `)
-    $('#coinsModal').modal('show')
+    `);
+    $("#coinsModal").modal("show");
   }
-}
+};
 
 // FUNCTION modalCollapse ADDING OPTION TO CHANGE COINS IN MODAL
 const modalCollapse = () => {
   // REMOVE ALL OPTIONS INSTEAD OF FIRST
-  $('.replaceCoin option:not(:first-child)').remove()
+  $(".replaceCoin option:not(:first-child)").remove();
   //  MAKE FIRST OPTION SELECTED
-  $('.replaceCoin').val($('.replaceCoin option:first').val())
+  $(".replaceCoin").val($(".replaceCoin option:first").val());
   // LOOP SELECTED COINS AND APPEND OPTIONS TO SELECT
   for (let i = 0; i < liveReportsArr.length - 1; i++) {
-    const option = $('<option>')
+    const option = $("<option>")
       .val(liveReportsArr[i].name)
-      .text(liveReportsArr[i].name)
-    $('.replaceCoin').append(option)
+      .text(liveReportsArr[i].name);
+    $(".replaceCoin").append(option);
   }
-}
+};
 
 // FUNCTION changeCoin FOR CHANGING THE COINS IN LIVE REPORT
 const changeCoin = () => {
   // CLOSE THE COLLAPSE
-  $('#coinsCollapse').collapse('hide')
+  $("#coinsCollapse").collapse("hide");
   // CLOSE THE MODAL
-  $('#coinsModal').modal('hide')
-  let checkBoxClass = '.checkbox-' + $('.replaceCoin').val()
+  $("#coinsModal").modal("hide");
+  const checkBoxClass = ".checkbox-" + $(".replaceCoin").val();
   // Find checkbox with the specified class and uncheck
-  $(checkBoxClass + ':checkbox').prop('checked', false)
+  $(checkBoxClass + ":checkbox").prop("checked", false);
   // REMOVE FROM ARRAY THE SELECTED COIN TO REMOVE
   for (let coin of liveReportsArr) {
-    if (coin.name === $('.replaceCoin').val()) {
-      liveReportsArr.splice(liveReportsArr.indexOf(coin), 1)
-      break
+    if (coin.name === $(".replaceCoin").val()) {
+      liveReportsArr.splice(liveReportsArr.indexOf(coin), 1);
+      break;
     }
   }
-}
+};
 
 // FUNCTION modalCancel IF USER WANT TO CLOSE THE MODAL AND CANCEL COIN CHANGE
 const modalCancel = () => {
   // CLOSE THE COLLAPSE
-  $('#coinsCollapse').collapse('hide')
+  $("#coinsCollapse").collapse("hide");
   // Find the last checked checkbox element and uncheck it
-  let lastCoin = liveReportsArr[liveReportsArr.length - 1].name.replaceAll(
-    ' ',
-    '.',
-  )
-  $(`.checkbox-${lastCoin}`).prop('checked', false)
+  const lastCoin = liveReportsArr[liveReportsArr.length - 1].name.replaceAll(
+    " ",
+    "."
+  );
+  $(`.checkbox-${lastCoin}`).prop("checked", false);
   // remove the coin from liveReportsArr
-  liveReportsArr.pop()
-}
+  liveReportsArr.pop();
+};
 
 // Function to show a specific section and hide all others
-const showSection = (section) => {
-  $('.card, #liveReports, #about').hide()
-  $(section).show()
-}
+const showSection = section => {
+  $(".card, #liveReports, #about").hide();
+  $(section).show();
+};
 
 // FUNCTION getCoinsPriceChart GETS THE COINS CURRENCIES FROM THE API FOR THE CHART
-const getCoinsPriceChart = async (start) => {
+const getCoinsPriceChart = async start => {
   // API
-  let cryptoChart = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms='
+  let cryptoChart = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=";
   // ADD SELECTED COIN TO API
-  const symbols = liveReportsArr.map((coin) => coin.symbol.toUpperCase())
-  cryptoChart += symbols.join(',')
-  cryptoChart += '&tsyms=USD'
+  const symbols = liveReportsArr.map(coin => coin.symbol.toUpperCase());
+  cryptoChart += symbols.join(",");
+  cryptoChart += "&tsyms=USD";
   return new Promise((resolve, reject) => {
     $.get({
       url: cryptoChart,
-      success: (data) => {
-        setTimeout(() => createChart(data, start), 1000)
-        resolve(data)
+      success: data => {
+        setTimeout(() => createChart(data, start), 1000);
+        resolve(data);
       },
-      error: (error) => {
-        reject(error)
+      error: error => {
+        reject(error);
       },
-    })
-  })
-}
-let dataPoints = [] // define an array to store the data points
+    });
+  });
+};
+let dataPoints = []; // define an array to store the data points
 
 // CREATE CHART
 const createChart = async (data, start) => {
-  let elapsedSeconds = (Date.now() - start) / 1000
-  elapsedSeconds = new Date(elapsedSeconds * 1000).toLocaleTimeString()
+  let elapsedSeconds = (Date.now() - start) / 1000;
+  elapsedSeconds = new Date(elapsedSeconds * 1000).toLocaleTimeString();
   // Get an array of coin symbols from the data object
-  const coinSymbols = Object.keys(data)
+  const coinSymbols = Object.keys(data);
   // Create an array of data point objects for each coin
-  coinSymbols.forEach((symbol) => {
-    const coinData = data[symbol]
-    const newDataPoint = { label: elapsedSeconds, y: coinData.USD }
+  coinSymbols.forEach(symbol => {
+    const coinData = data[symbol];
+    const newDataPoint = { label: elapsedSeconds, y: coinData.USD };
     const existingDataPoints =
-      dataPoints.find((dp) => dp.name === symbol)?.dataPoints || []
-    existingDataPoints.push(newDataPoint)
-    dataPoints = dataPoints.filter((dp) => dp.name !== symbol)
+      dataPoints.find(dp => dp.name === symbol)?.dataPoints || [];
+    existingDataPoints.push(newDataPoint);
+    dataPoints = dataPoints.filter(dp => dp.name !== symbol);
     dataPoints.push({
-      type: 'line',
+      type: "line",
       name: symbol,
       showInLegend: true,
       dataPoints: existingDataPoints,
-    })
-  })
+    });
+  });
 
   // Remove any symbols that are not present in liveReportsArr
-  dataPoints = dataPoints.filter((dp) =>
-    liveReportsArr.some((coin) => coin.symbol.toUpperCase() === dp.name),
-  )
+  dataPoints = dataPoints.filter(dp =>
+    liveReportsArr.some(coin => coin.symbol.toUpperCase() === dp.name)
+  );
 
   // Create the chart object with the data points array
   const chart = {
     title: {
-      text: 'Crypto Prices In USD',
-      fontColor: '#ffc107',
+      text: "Crypto Prices In USD",
+      fontColor: "#ffc107",
     },
-    backgroundColor: 'black',
+    backgroundColor: "black",
     data: dataPoints,
     axisY: {
-      labelFontColor: '#ffc107',
+      labelFontColor: "#ffc107",
     },
     axisX: {
-      labelFontColor: '#ffc107',
+      labelFontColor: "#ffc107",
     },
-  }
+  };
 
   // Update the chart
-  $('#liveReportChart').CanvasJSChart(chart)
-}
+  $("#liveReportChart").CanvasJSChart(chart);
+};
